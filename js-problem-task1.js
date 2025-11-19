bc5c6069131ed1ab288c8a5425d38a87767d22c9
Rakib Hasan <rakibhasan1808@gmail.com>
2025-11-19 12:21:38 +0600

    added Js-problems-task4.js file

--- js-problem-task1.js
+++ js-problem-task1.js
@@ -8,7 +8,7 @@ const heights2 = [167, 190, 120, 165, 137];
 const lowest = Math.min(...heights2);
 console.log ("lowest number is:", lowest);
 
-//another option লুপ দিয়ে
+//another option while  লুপ দিয়ে
 
 let i = 0;
 let lowest1 = heights2[0];
