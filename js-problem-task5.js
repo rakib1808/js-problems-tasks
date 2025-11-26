f9df5720da09d0d68b452d583a851406f4272a9c
Rakib Hasan <rakibhasan1808@gmail.com>
2025-11-27 01:44:56 +0600

    update Js-problems-task6.js file

--- js-problem-task5.js
+++ js-problem-task5.js
@@ -26,25 +26,6 @@ console.log(odd);
 
 
 
-//*******Task-02, odd even.js**********
-
-            // Subtask-1:
-// Find all the odd numbers from 61 to 100.
-
-for (let numbers = 61; numbers <=100 ; numbers++) {
-    if (numbers % 2 === 1) {
-        console.log('the odd number is:', numbers );
-    }
-}
-
-            // Subtask-2:
-// Find all the even numbers from 78 to 98.
-
-for (let number = 78; number <=98; number++){
-    if (number % 2=== 0) {
-        console.log('the even number is:', number);
-    }
-}
 
 
 //***********Task-03, sum of odd numbers.js***********
--- js-problem-task6.js
+++ js-problem-task6.js
@@ -12,3 +12,24 @@ for (let message = 1; message <= 60; message++) {
 }
 
 
+//*******Task-02, odd even.js**********
+
+            // Subtask-1:
+// Find all the odd numbers from 61 to 100.
+
+for (let numbers = 61; numbers <=100 ; numbers++) {
+    if (numbers % 2 === 1) {
+        console.log('the odd number is:', numbers );
+    }
+}
+
+            // Subtask-2:
+// Find all the even numbers from 78 to 98.
+
+for (let number = 78; number <=98; number++){
+    if (number % 2=== 0) {
+        console.log('the even number is:', number);
+    }
+}
+
+
