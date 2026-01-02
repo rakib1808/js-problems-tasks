356135deec9eb360f188a7c2c5f81cac5c82c119
Rakib Hasan <rakibhasan1808@gmail.com>
2026-01-03 00:27:37 +0600

    added Js-problems-task7.js file

---
+++ js-problem-task7.js
@@ -0,0 +1,6 @@
+/*            *Condition Practice* 
+
+            "Task-01"
+ * Your mom said she will give you snacks if you return home before 6 o’clock. But if you’re late, she will scold you.
+
+*/
